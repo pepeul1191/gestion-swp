e9356bea2cf7b9659c269e3ff73fa451fcbfc988
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-26 17:01:38 -0500

    Renderizando primera vista

--- handlers/login.py
+++ handlers/login.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tornado.web
-import requests
-import datetime
-import json
 from config.base import BaseHandler
 
 class LoginIndex(BaseHandler):
   def get(self):
     self.set_status(400)
-    self.write("<h1>LOGIN</h1>")
+    self.render('handlers/login.html', title='Login')
