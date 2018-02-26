aacb292dec1f2b1a158a984d92321b924348a32f
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-26 17:07:09 -0500

    Uso de config/constants.py en las vistas

--- handlers/login.py
+++ handlers/login.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import tornado.web
+from config.constants import constants
 from config.base import BaseHandler
 
 class LoginIndex(BaseHandler):
   def get(self):
-    self.set_status(400)
-    self.render('handlers/login.html', title='Login')
+    self.set_status(200)
+    self.render('handlers/login.html', constants= constants, title= 'Login')
