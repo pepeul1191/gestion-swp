b601f44cbdf39e1f3f2b49fdf4d437064e101a7f
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-06 21:58:05 -0500

    Agregando archivos rb para preubas rspec

--- config/database.py
+++ config/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-
+# Base de datos sql relacional - ubicaciones
 engine_ubicaciones = create_engine('sqlite:///db/ubicaciones.db')
 session_ubicaciones = sessionmaker()
 session_ubicaciones.configure(bind = engine_ubicaciones)
-
+# Base de datos sql relacional - accesos
 engine_accesos = create_engine('sqlite:///db/db_accesos.db')
 session_accesos = sessionmaker()
 session_accesos.configure(bind = engine_accesos)
